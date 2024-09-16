85e0d202fdbe166d58f06b3497816008f34c8e4b
Suraj SR <suraj.sadasivanrajalekshmi@lamresearch.com>
2024-09-16 03:28:01 +0000

    My first stable version of code

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/fe/core/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("trav.proc.travelprocessor.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
